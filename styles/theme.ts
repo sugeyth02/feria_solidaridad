1f0db7d420dd3883aedd60f77461820769025342
Sugeyth Rodriguez <d.sugeyth02@gmail.com>
2023-02-14 20:32:05 -0600

    :mag: Update SEO

---
+++ styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+    colors: {
+        blue: '#003C71',
+        black: '#000000',
+        yellow: '#F2CF66',
+        lightOrange: '#F28966',
+    },
+    breakpoints: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1440px',
+        xxxl: '1680px',
+    },
+}
